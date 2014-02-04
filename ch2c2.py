98699293a5fbc6ded51d8447505ce12ccc1f70d9
jrex21 <jrex@g.clemson.edu>
2014-02-03 22:55:37 -0500

    Python book chapter 2 challenges

---
+++ ch2c2.py
@@ -0,0 +1,9 @@
+# Chapter 2
+# Challenge 2
+
+food1 = input("What's your favorite food? ")
+food2 = input("What's your second favorite food? ")
+
+newfood = food1 + food2
+
+print("Your new food is:", newfood)
---
+++ ch2c3.py
@@ -0,0 +1,11 @@
+# Tipper program
+# Chapter 2
+# Challenge 3
+
+bill_total = float(input("What is your bill total? "))
+
+print("15% tip: ", float(bill_total * .15))
+print("20% tip: ", float(bill_total * .20))
+
+input("\n\nPress the enter key to exit.")
+                   
